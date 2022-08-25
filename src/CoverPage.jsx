a856ab2745863160ce7b7e6881e1869141dc3cb2
nicholasburgher <55260105+nicholasburgher@users.noreply.github.com>
2022-08-24 20:44:40 -0500

    Initial Commit

--- src/CoverPage.jsx
+++ src/CoverPage.jsx
@@ -5,7 +5,7 @@ function CoverPage() {
   return (
     <div class="cover-page">
       <div style={{ textAlign: "center" }}>
-        <h1>UX / UI Designer</h1>
+        <h1>Designer</h1>
         <h2>Nicholas Burgher</h2>
       </div>
       <h3 onClick={() => navigate('work')} style={{ cursor: "pointer" }}>ENTER</h3>
