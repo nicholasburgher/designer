de40b1e716c46498c7261c09f10f48e39254672f
nicholasburgher <55260105+nicholasburgher@users.noreply.github.com>
2023-01-30 13:54:21 -0600

    reimplementation - video tag playsinline function

--- src/components/blocks/Video.jsx
+++ src/components/blocks/Video.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 function Video(props) {
     return (
-        <video style={{display: "grid", width: "100%", maxWidth: "700px", padding: "1rem 0", margin: "0 auto"}} autoPlay={props.autoplay === "autoplay" ? "autoplay": "none"} muted={props.autoplay ? "false": "true"} controls playsinline loop>
+        <video style={{display: "grid", width: "100%", maxWidth: "700px", padding: "1rem 0", margin: "0 auto"}} autoPlay={props.autoplay === "autoplay" ? "autoplay": "none"} muted={props.autoplay ? "false": "true"} controls playsInline loop>
             <source src={process.env.PUBLIC_URL + "/assets/videos/" + props.source} type="video/mp4" />
             Your browser does not support video tags.
         </video>
